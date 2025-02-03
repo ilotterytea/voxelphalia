1696a58a9af34fd6b254adde14e355845ec99ce4
ilotterytea <iltsu@alright.party>
2025-02-03 20:04:43 +0500

    feat: level

---
+++ core/src/main/java/kz/ilotterytea/voxelphalia/level/Level.java
@@ -0,0 +1,61 @@
+package kz.ilotterytea.voxelphalia.level;
+
+import com.badlogic.gdx.utils.Array;
+
+public class Level {
+    protected final Array<Chunk> chunks;
+    protected final int width, height, depth;
+
+    public Level(int width, int height, int depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+
+        int chunkCapacity = width * height * depth;
+        this.chunks = new Array<>(chunkCapacity);
+
+        for (int i = 0; i < chunkCapacity; i++) {
+            this.chunks.add(new Chunk(16, 16, 16));
+        }
+    }
+
+    public byte getVoxel(int x, int y, int z) {
+        Chunk chunk = getChunk(x, y, z);
+        int cx = x % 16;
+        int cy = y % 16;
+        int cz = z % 16;
+        return chunk.getVoxel(cx, cy, cz);
+    }
+
+    public void placeVoxel(byte voxel, int x, int y, int z) {
+        Chunk chunk = getChunk(x, y, z);
+        int cx = x % 16;
+        int cy = y % 16;
+        int cz = z % 16;
+        chunk.placeVoxel(voxel, cx, cy, cz);
+    }
+
+    public Chunk getChunk(int x, int y, int z) {
+        return this.chunks.get(getChunkIndex(x, y, z));
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    private int getChunkIndex(int worldX, int worldY, int worldZ) {
+        int cx = worldX / 16;
+        int cy = worldY / 16;
+        int cz = worldZ / 16;
+
+        return cx + cz * width + cy * width * depth;
+    }
+}
