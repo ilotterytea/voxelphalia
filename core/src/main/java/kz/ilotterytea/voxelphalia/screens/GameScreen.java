5268fdce4c59f8dcc5709272adf2e355013bb0fe
ilotterytea <me@ilotterytea.kz>
2025-02-04 20:22:04 +0500

    feat: setup 3d

--- core/src/main/java/kz/ilotterytea/voxelphalia/screens/GameScreen.java
+++ core/src/main/java/kz/ilotterytea/voxelphalia/screens/GameScreen.java
@@ -1,40 +1,73 @@
 package kz.ilotterytea.voxelphalia.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
+import com.badlogic.gdx.graphics.g3d.utils.DefaultShaderProvider;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GameScreen implements Screen {
+    private PerspectiveCamera camera;
+    private ModelBatch modelBatch;
+    private Environment environment;
+
     @Override
     public void show() {
+        camera = new PerspectiveCamera(60, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.near = 0.1f;
+        camera.far = 100f;
+        camera.update();
 
+        DefaultShader.Config config = new DefaultShader.Config();
+        config.defaultCullFace = GL20.GL_FRONT;
+        DefaultShaderProvider modelBatchProvider = new DefaultShaderProvider(config);
+        modelBatch = new ModelBatch(modelBatchProvider);
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1.f));
+        environment.set(new ColorAttribute(ColorAttribute.Fog, 0.8f, 0.8f, 0.8f, 0.8f));
+        environment.add(new DirectionalLight().set(1, 1, 1, 0, -1, 0));
     }
 
     @Override
     public void render(float delta) {
+        ScreenUtils.clear(Color.SKY, true);
 
+        modelBatch.begin(camera);
+        modelBatch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.update();
     }
 
     @Override
     public void pause() {
-
+        hide();
     }
 
     @Override
     public void resume() {
-
+        show();
     }
 
     @Override
     public void hide() {
-
+        dispose();
     }
 
     @Override
     public void dispose() {
-
+        modelBatch.dispose();
     }
 }
