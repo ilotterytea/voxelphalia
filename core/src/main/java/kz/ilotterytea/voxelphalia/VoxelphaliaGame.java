b33c3fc9b895f2e1ebc8de5859d1eabe1a72365d
ilotterytea <iltsu@alright.party>
2025-02-05 20:52:33 +0500

    feat: asset manager

--- core/src/main/java/kz/ilotterytea/voxelphalia/VoxelphaliaGame.java
+++ core/src/main/java/kz/ilotterytea/voxelphalia/VoxelphaliaGame.java
@@ -1,7 +1,8 @@
 package kz.ilotterytea.voxelphalia;
 
 import com.badlogic.gdx.Game;
-import kz.ilotterytea.voxelphalia.screens.GameScreen;
+import com.badlogic.gdx.assets.AssetManager;
+import kz.ilotterytea.voxelphalia.screens.SplashScreen;
 
 /**
  * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
@@ -9,13 +10,21 @@ import kz.ilotterytea.voxelphalia.screens.GameScreen;
 public class VoxelphaliaGame extends Game {
     private static VoxelphaliaGame instance;
 
+    private AssetManager assetManager;
+
     @Override
     public void create() {
-        setScreen(new GameScreen());
+        assetManager = new AssetManager();
+
+        setScreen(new SplashScreen());
     }
 
     public static VoxelphaliaGame getInstance() {
         if (instance == null) instance = new VoxelphaliaGame();
         return instance;
+    }
+
+    public AssetManager getAssetManager() {
+        return assetManager;
     }
 }
---
+++ core/src/main/java/kz/ilotterytea/voxelphalia/screens/SplashScreen.java
@@ -0,0 +1,53 @@
+package kz.ilotterytea.voxelphalia.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.ScreenUtils;
+import kz.ilotterytea.voxelphalia.VoxelphaliaGame;
+import kz.ilotterytea.voxelphalia.assets.AssetUtils;
+
+public class SplashScreen implements Screen {
+    private VoxelphaliaGame game;
+
+    @Override
+    public void show() {
+        game = VoxelphaliaGame.getInstance();
+        AssetUtils.queue(game.getAssetManager());
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(Color.WHITE);
+
+        if (game.getAssetManager().update()) {
+            Gdx.app.log(SplashScreen.class.getSimpleName(), "Loaded all assets");
+            game.setScreen(new GameScreen());
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+        hide();
+    }
+
+    @Override
+    public void resume() {
+        show();
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
