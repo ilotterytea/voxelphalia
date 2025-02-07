7a7d6d1dd28d07b508f35922212bbdc584369e30
ilotterytea <me@ilotterytea.kz>
2025-02-07 22:40:18 +0500

    feat: metadata info

---
+++ core/src/main/java/kz/ilotterytea/voxelphalia/VoxelphaliaConstants.java
@@ -0,0 +1,8 @@
+package kz.ilotterytea.voxelphalia;
+
+public class VoxelphaliaConstants {
+    public static class Metadata {
+        public static final String APP_NAME = "voxelphalia";
+        public static final String APP_VERSION = "0.1.0";
+    }
+}
