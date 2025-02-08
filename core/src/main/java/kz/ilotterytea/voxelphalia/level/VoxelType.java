b928c6ac9fdcdb68023e8d54b4b3207ab3904503
ilotterytea <iltsu@alright.party>
2025-02-08 18:26:04 +0500

    feat: new voxels

--- core/src/main/java/kz/ilotterytea/voxelphalia/level/VoxelType.java
+++ core/src/main/java/kz/ilotterytea/voxelphalia/level/VoxelType.java
@@ -6,7 +6,13 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public enum VoxelType {
     AIR(-1, -1),
     GRASS(0, 0),
-    ROCK(1, 0);
+    ROCK(1, 0),
+    DIRT(2, 0),
+    SAND(3, 0),
+    MANTLE(4, 0),
+    SNOW(5, 0),
+    WATER(0, 15),
+    MISSING_VOXEL(15, 15);
 
     private final int offsetX, offsetY;
 
@@ -24,6 +30,10 @@ public enum VoxelType {
     }
 
     public static VoxelType getById(byte voxel) {
+        if (voxel >= values().length) {
+            return VoxelType.MISSING_VOXEL;
+        }
+
         return values()[voxel];
     }
 
