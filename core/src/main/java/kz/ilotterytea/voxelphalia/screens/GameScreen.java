074f9b2dac9eab25ff884bb2a8adb0eedc5deafe
ilotterytea <me@ilotterytea.kz>
2025-02-08 15:08:29 +0500

    feat: tree generation

--- core/src/main/java/kz/ilotterytea/voxelphalia/screens/GameScreen.java
+++ core/src/main/java/kz/ilotterytea/voxelphalia/screens/GameScreen.java
@@ -8,21 +8,28 @@ import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.decals.CameraGroupStrategy;
+import com.badlogic.gdx.graphics.g3d.decals.DecalBatch;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
 import com.badlogic.gdx.graphics.g3d.utils.DefaultShaderProvider;
 import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.github.czyzby.noise4j.map.Grid;
 import com.github.czyzby.noise4j.map.generator.noise.NoiseGenerator;
-import com.github.czyzby.noise4j.map.generator.util.Generators;
 import kz.ilotterytea.voxelphalia.VoxelphaliaGame;
+import kz.ilotterytea.voxelphalia.entities.RenderableEntity;
+import kz.ilotterytea.voxelphalia.entities.TreeEntity;
 import kz.ilotterytea.voxelphalia.level.Level;
 import kz.ilotterytea.voxelphalia.level.RenderableLevel;
 import kz.ilotterytea.voxelphalia.ui.DebugInfoTable;
+
+import java.util.Random;
 
 public class GameScreen implements Screen {
     private VoxelphaliaGame game;
@@ -36,6 +43,9 @@ public class GameScreen implements Screen {
 
     private Level level;
     private RenderableLevel renderableLevel;
+
+    private DecalBatch decalBatch;
+    private Array<RenderableEntity> renderableEntities;
 
     @Override
     public void show() {
@@ -59,17 +69,19 @@ public class GameScreen implements Screen {
         environment.set(new ColorAttribute(ColorAttribute.Fog, 0.8f, 0.8f, 0.8f, 0.8f));
         environment.add(new DirectionalLight().set(1, 1, 1, 0, -1, 0));
 
+        int seed = (int) (System.currentTimeMillis() / 1000);
+
         level = new Level(40, 4, 40);
         renderableLevel = new RenderableLevel(camera, level);
 
         // terrain generation
         NoiseGenerator noiseGenerator = new NoiseGenerator();
         Grid grid = new Grid(level.getWidthInVoxels(), level.getDepthInVoxels());
-        noiseStage(grid, noiseGenerator, 32, 0.6f);
-        noiseStage(grid, noiseGenerator, 16, 0.2f);
-        noiseStage(grid, noiseGenerator, 8, 0.1f);
-        noiseStage(grid, noiseGenerator, 4, 0.1f);
-        noiseStage(grid, noiseGenerator, 1, 0.05f);
+        noiseStage(grid, noiseGenerator, 32, 0.6f, seed);
+        noiseStage(grid, noiseGenerator, 16, 0.2f, seed);
+        noiseStage(grid, noiseGenerator, 8, 0.1f, seed);
+        noiseStage(grid, noiseGenerator, 4, 0.1f, seed);
+        noiseStage(grid, noiseGenerator, 1, 0.05f, seed);
 
         for (int x = 0; x < grid.getWidth(); x++) {
             for (int z = 0; z < grid.getHeight(); z++) {
@@ -101,6 +113,20 @@ public class GameScreen implements Screen {
         stage = new Stage(new ScreenViewport());
         Skin skin = game.getAssetManager().get("textures/gui/gui.skin");
         stage.addActor(new DebugInfoTable(skin, renderableLevel));
+
+        decalBatch = new DecalBatch(new CameraGroupStrategy(camera));
+        renderableEntities = new Array<>();
+
+        Random random = new Random(seed);
+
+        // tree generation
+        for (int i = 0; i < random.nextInt(800, 2000); i++) {
+            int x = random.nextInt(0, level.getWidthInVoxels());
+            int z = random.nextInt(0, level.getDepthInVoxels());
+
+            TreeEntity entity = new TreeEntity(new Vector3(x, level.getHighestY(x, z) + 2, z));
+            renderableEntities.add(entity);
+        }
     }
 
     @Override
@@ -114,6 +140,12 @@ public class GameScreen implements Screen {
         modelBatch.begin(camera);
         renderableLevel.render(modelBatch, environment);
         modelBatch.end();
+
+        for (RenderableEntity entity : renderableEntities) {
+            entity.tick(delta, camera);
+            entity.render(decalBatch);
+        }
+        decalBatch.flush();
 
         stage.act(delta);
         stage.draw();
@@ -147,14 +179,15 @@ public class GameScreen implements Screen {
     public void dispose() {
         stage.dispose();
         renderableLevel.dispose();
+        decalBatch.dispose();
         modelBatch.dispose();
     }
 
     private void noiseStage(final Grid grid, final NoiseGenerator noiseGenerator, final int radius,
-                            final float modifier) {
+                            final float modifier, int seed) {
         noiseGenerator.setRadius(radius);
         noiseGenerator.setModifier(modifier);
-        noiseGenerator.setSeed(Generators.rollSeed());
+        noiseGenerator.setSeed(seed);
         noiseGenerator.generate(grid);
     }
 }
