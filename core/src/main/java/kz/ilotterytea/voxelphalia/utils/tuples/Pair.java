99ec145151bffc0ea3f14d76cba0a541ccdb2588
ilotterytea <iltsu@alright.party>
2025-02-09 21:44:44 +0500

    feat: multi-thread chunk generation

---
+++ core/src/main/java/kz/ilotterytea/voxelphalia/utils/tuples/Pair.java
@@ -0,0 +1,11 @@
+package kz.ilotterytea.voxelphalia.utils.tuples;
+
+public class Pair<A, B> {
+    public A first;
+    public B second;
+
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+}
