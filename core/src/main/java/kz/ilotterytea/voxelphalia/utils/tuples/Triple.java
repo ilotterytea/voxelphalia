41052f3c32905dca974f2325e00c996809a2c4db
ilotterytea <me@ilotterytea.kz>
2025-02-17 23:11:56 +0500

    feat: class for triples

---
+++ core/src/main/java/kz/ilotterytea/voxelphalia/utils/tuples/Triple.java
@@ -0,0 +1,13 @@
+package kz.ilotterytea.voxelphalia.utils.tuples;
+
+public class Triple<A, B, C> {
+    public A first;
+    public B second;
+    public C third;
+
+    public Triple(A first, B second, C third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
