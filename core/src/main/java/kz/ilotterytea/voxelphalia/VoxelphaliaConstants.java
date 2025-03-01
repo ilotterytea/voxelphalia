fdd92bdb6c3ebc49828a4564f309cff31a710af4
ilotterytea <me@ilotterytea.kz>
2025-03-02 03:38:07 +0500

    feat: developer constant

--- core/src/main/java/kz/ilotterytea/voxelphalia/VoxelphaliaConstants.java
+++ core/src/main/java/kz/ilotterytea/voxelphalia/VoxelphaliaConstants.java
@@ -5,6 +5,7 @@ public class VoxelphaliaConstants {
         public static final String APP_NAME = "voxelphalia";
         public static final String APP_ID = "voxelphalia";
         public static final String APP_PACKAGE = "kz.ilotterytea.voxelphalia";
+        public static final String APP_DEV = "ilotterytea";
         public static final String APP_VERSION = "0.7";
     }
 }
